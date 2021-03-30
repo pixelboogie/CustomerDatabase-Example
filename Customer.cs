fcd9b106c46707c8baed7ddb84ec4ed1c8b5f72e
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-30 13:14:44 -0700

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Customer
+{
+
+    public string firstName;
+    public int age;
+
+    public Customer(string fn, int a)
+    {
+
+        this.firstName = fn;
+        this.age = a;
+
+        Debug.Log("Customer ---> " + firstName + " age " + age);
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ CustomerDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerDatabase : MonoBehaviour
+{
+    
+    public Customer cust1;
+    public Customer cust2;
+    public Customer cust3;
+
+    public Customer[] customers;
+
+    void Start()
+    {
+        cust1 = new Customer("Jonathon", 10);
+        cust2 = new Customer("Jannet", 11);
+        cust3 = new Customer("Joe", 12);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
